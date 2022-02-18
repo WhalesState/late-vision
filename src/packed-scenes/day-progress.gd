6143de820a654f3eda6046643fc2b3e7026918ce
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2022-02-18 18:34:10 +0200

    update day-progress.gd

--- src/packed-scenes/day-progress.gd
+++ src/packed-scenes/day-progress.gd
@@ -1,14 +1,19 @@
 tool
 extends NinePatchRect
 
-var update := false
+export(int, 1, 1440, 1) var time = 1
+
+onready var progress_material : ShaderMaterial = get_child(0).get_child(0).material
 
 
 func _ready() -> void:
 	assert(OK == connect("resized", self, "on_resize"))
-	pass # Replace with function body.
 
 
 func on_resize() -> void:
-	get_child(0).get_child(0).material.set_shader_param("size", rect_size)
+	progress_material.set_shader_param("size", rect_size - Vector2(12, 12))
 
+
+func set_time(_time: float) -> void:
+	time = _time
+	progress_material.set_shader_param("time", clamp(time, 0, 1440))
